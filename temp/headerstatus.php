079b357ee60fbe1308418bab1928aefa7eaf74cf
rusdiansyah <rusdiansyah13@mhs.is.its.ac.id>
2014-12-28 16:43:19 +0700

    headerstatus.php
    header status

---
+++ temp/headerstatus.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" href="images/logo.png" type="image/x-icon">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<title><?php echo $_SESSION['username'];?>'s Profile</title>
+</head>
+<body>
+	<header>
+		<div id="logo">
+			<a href="#"><img src="images/logo.png" width="50px" height="50px"></a>
+			<div><strong>RUMAH SAKIT PELITA HARAPAN</strong></div>
+		
+		</div>
+		<div id="nav">
+			<ul>
+				<li><a href="logout.php">LOG OUT</a></li>
+				<li><a href="profile.php">HOME</a></li>
+				<li><a href="cari.php">CARI PASIEN</a></li>				
+			</ul>
+		</div>
+		<div class="clear"></div>
+	</header>
